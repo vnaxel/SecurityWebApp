01ad6618817a1710df8ab1e1381f91173886b5de
Axel Yvenou <axel.yvenou@gmail.com>
2021-09-25 18:12:37 +0200

    created data set for request

--- src/main/java/fr/training/securitywebapp/utils/DataDAO.java
+++ src/main/java/fr/training/securitywebapp/utils/DataDAO.java
@@ -1,5 +1,36 @@
 package fr.training.securitywebapp.utils;
 
-public class DataDAO {
+import java.util.HashMap;
+import java.util.Map;
 
+import fr.training.securitywebapp.bean.UserAccount;
+
+public class DataDAO {
+	
+	private static final Map<String, UserAccount> mapUsers = new HashMap<String, UserAccount>();
+	
+	static {
+		initUsers();
+	}
+	
+	private static void initUsers() {
+		
+		UserAccount emp = new UserAccount("employee1", "123", UserAccount.GENDER_MALE);
+		
+		UserAccount mng = new UserAccount("manager1", "123", UserAccount.GENDER_MALE);
+		
+		mapUsers.put(emp.getUserName(), emp);
+		mapUsers.put(mng.getUserName(), mng);
+	}
+	
+	public static UserAccount findUser(String userName, String password) {
+		UserAccount u = mapUsers.get(userName);
+		if (u != null && u.getPassword().equals(password)) {
+			return u;
+		} else {
+			return null;
+		}
+		
+	}
+	
 }
