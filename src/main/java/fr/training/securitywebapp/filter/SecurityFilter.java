a840ee37ca9561f7b880694055b3248119d1a69a
Axel Yvenou <axel.yvenou@gmail.com>
2021-09-28 17:33:05 +0200

    corrected missing WebFilter import

--- src/main/java/fr/training/securitywebapp/filter/SecurityFilter.java
+++ src/main/java/fr/training/securitywebapp/filter/SecurityFilter.java
@@ -1,7 +1,6 @@
 package fr.training.securitywebapp.filter;
 
 import java.io.IOException;
-
 import java.util.List;
 
 import fr.training.securitywebapp.bean.UserAccount;
@@ -19,69 +18,70 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
 @WebFilter("/*")
 public class SecurityFilter implements Filter {
-
+	
 	public SecurityFilter() {
 	}
 
 	@Override
 	public void destroy() {
 	}
-
+	
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
-			throws IOException, ServletException {
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+	throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) resp;
-
+		HttpServletResponse response = (HttpServletResponse) res;
+		
 		String servletPath = request.getServletPath();
-
+		
 		UserAccount loginedUser = AppUtils.getLoginedUser(request.getSession());
-
-		if (servletPath.equals("/login")) {
+		
+		if(servletPath.equals("/login")) {
 			chain.doFilter(request, response);
 			return;
 		}
 		HttpServletRequest wrapRequest = request;
-
+		
+		
 		if (loginedUser != null) {
 			String userName = loginedUser.getUserName();
-
+			
 			List<String> roles = loginedUser.getRoles();
-
+			
 			wrapRequest = new UserRoleRequestWrapper(userName, roles, request);
 		}
-
+		
 		if (SecurityUtils.isSecurityPage(request)) {
-
+			
 			if (loginedUser == null) {
-
+				
 				String requestUri = request.getRequestURI();
-
+				
+				//Stocke la page à rediriger après la connexion
 				int redirectId = AppUtils.storeRedirectAfterLoginUrl(request.getSession(), requestUri);
-
-				response.sendRedirect(wrapRequest.getContextPath() + "/login?redirectId=" + redirectId);
+				
+				response.sendRedirect(wrapRequest.getContextPath()+ "/login?redirectId=" + redirectId);
 				return;
 			}
-
+			
 			boolean hasPermission = SecurityUtils.hasPermission(wrapRequest);
-			if (!hasPermission) {
-
-				RequestDispatcher dispatcher //
-						= request.getServletContext().getRequestDispatcher("/WEB-INF/views/accessDeniedView.jsp");
-
+			if(!hasPermission) {
+				
+				RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/WEB-INF/views/accesDeniedView.jsp");
+				
 				dispatcher.forward(request, response);
 				return;
 			}
 		}
-
+		
 		chain.doFilter(wrapRequest, response);
 	}
-
+	
 	@Override
 	public void init(FilterConfig fConfig) throws ServletException {
-
 	}
-
+	
 }
